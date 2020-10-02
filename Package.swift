bc439cdc29457af520a4d8214ccf520303f691d2
Michal A <dev@pokryfka.net>
2020-10-02 15:30:55 +0800

    chore: update pure-swift-json

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.2.1")),
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.22.0")),
-        .package(url: "https://github.com/fabianfett/pure-swift-json.git", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/fabianfett/pure-swift-json.git", .upToNextMinor(from: "0.5.0")),
     ],
     targets: [
         .target(
